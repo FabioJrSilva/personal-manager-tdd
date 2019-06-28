cd8be0fd4fa8729916be1a82118994d829e6b7e9
Fabio <fabio.junior@uotz.com.br>
2019-06-28 18:15:30 -0300

    Criação dos teste de criação de usuários

--- src/services/user.js
+++ src/services/user.js
@@ -1,9 +1,16 @@
 module.exports = (app) => {
-  const findAll = () => {
-    return app.db('users').select();
+  const findAll = (filter = {}) => {
+    return app.db('users').where(filter).select();
   };
 
-  const create = (user) => {
+  const create = async (user) => {
+    if (!user.name) return { error: 'Nome é obrigatório!' };
+    if (!user.mail) return { error: 'Email é obrigatório!' };
+    if (!user.password) return { error: 'Password é obrigatório!' };
+
+    const userDb = await findAll({ mail: user.mail });
+    if (userDb && userDb.length) return { error: 'Email já existe!' };
+
     return app.db('users').insert(user, '*');
   };
 
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,21 +1,55 @@
 const request = require('supertest');
 const app = require('../../src/app');
 
+const mail = `${Date.now()}@exemplo.com`;
+
 describe('User', () => {
-  test('should list all users', () => {
+  it('should list all users', () => {
     return request(app).get('/users').then((res) => {
       expect(res.status).toBe(200);
       expect(res.body.length).toBeGreaterThan(0);
     });
   });
 
-  test('should insert new user', () => {
-    const mail = `${Date.now()}@exemplo.com`;
-    const user = { name: 'Fabio', mail, password: '010203' };
+  it('should insert new user', () => {
 
-    return request(app).post('/create').send(user).then((res) => {
-      expect(res.status).toBe(201);
-      expect(res.body.name).toEqual('Fabio');
-    });
+    return request(app).post('/create')
+      .send({ name: 'Fabio', mail, password: '010203' }).then((res) => {
+        expect(res.status).toBe(201);
+        expect(res.body.name).toEqual('Fabio');
+      });
+  });
+
+  it('should not create user without name', () => {
+    return request(app).post('/create')
+      .send({ mail, password: '010203' }).then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Nome é obrigatório!');
+      });
+  });
+
+  it('should not create user without mail', async () => {
+    const user = await request(app).post('/create')
+      .send({ name: 'Fabio', password: '010203' });
+    expect(user.status).toBe(400);
+    expect(user.body.error).toBe('Email é obrigatório!');
+  });
+
+  it('should not create user without password', (done) => {
+    request(app).post('/create')
+      .send({ name: 'Fabio', mail }).then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Password é obrigatório!');
+        done();
+      })
+      .catch(error => done.fail(error));
+  });
+
+  it('should not create user without mail exist', () => {
+    return request(app).post('/create')
+      .send({ name: 'Fabio', mail: 'fabio@exemplo.com', password: '010203' }).then((res) => {
+        expect(res.status).toBe(400);
+        expect(res.body.error).toBe('Email já existe!');
+      });
   });
 });
